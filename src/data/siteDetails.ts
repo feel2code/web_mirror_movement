42f13c2a77bfca37a356f7cf25af3de86f68df2c
feel2code <feel2code@proton.me>
2025-03-21 19:19:39 +0400

    google analytics

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,6 +1,6 @@
 export const siteDetails = {
     siteName: 'Зеркальный переход',
-    siteUrl: 'https://feel2code-web-mirror-movement-2c54.twc1.net',
+    siteUrl: 'https://feel2code-web-mirror-movement-798c.twc1.net',
     metadata: {
         title: 'Зеркальный Переход - ламповый VPN сервис',
         description: 'Зеркальный Переход - ламповый VPN сервис',
@@ -8,5 +8,5 @@ export const siteDetails = {
     language: 'ru-ru',
     locale: 'ru-RU',
     siteLogo: `${process.env.BASE_PATH || ''}/images/logo.ico`, // or use a string for the logo e.g. "TechStartup"
-    googleAnalyticsId: '', // e.g. G-XXXXXXX,
+    googleAnalyticsId: 'G-C4CTKWKWQ7', // e.g. G-XXXXXXX,
 }
